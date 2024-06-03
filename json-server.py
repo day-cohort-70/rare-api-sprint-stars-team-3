0cecec5ba8f26eae1ac85a4ef0c18bb2877bc7c5
Danny Hidalgo <dnnysongs@gmail.com>
2024-06-03 13:17:04 -0500

    expanded categories

--- json-server.py
+++ json-server.py
@@ -3,7 +3,15 @@ from http.server import HTTPServer
 from nss_handler import HandleRequests, status
 
 
-from views import retrieve_post, list_posts, login_user, create_category
+from views import (
+    retrieve_post,
+    list_posts,
+    login_user,
+    create_category,
+    list_categories,
+    delete_category,
+    update_category,
+)
 
 
 class JSONServer(HandleRequests):
@@ -17,6 +25,13 @@ class JSONServer(HandleRequests):
                 response_body = retrieve_post(url)
                 return self.response(response_body, status.HTTP_200_SUCCESS.value)
             response_body = list_posts(url)
+            return self.response(response_body, status.HTTP_200_SUCCESS.value)
+
+        if url["requested_resource"] == "categories":
+            if url["pk"] != 0:
+                response_body = ""
+                return self.response(response_body, status.HTTP_200_SUCCESS.value)
+            response_body = list_categories(url)
             return self.response(response_body, status.HTTP_200_SUCCESS.value)
 
     def do_POST(self):
@@ -51,8 +66,43 @@ class JSONServer(HandleRequests):
                 "Requested resource not found",
                 status.HTTP_500_SERVER_ERROR.value,
             )
+
+    def do_DELETE(self):
+        """Handle DELETE requests from a client"""
+        url = self.parse_url(self.path)
+        pk = url["pk"]
+
+        if url["requested_resource"] == "categories":
+            if pk != 0:
+                successfully_posted = delete_category(pk)
+                if successfully_posted:
+                    return self.response(
+                        "", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value
+                    )
+
         else:
             self.send_error(404)
+
+    def do_PUT(self):
+        url = self.parse_url(self.path)
+        pk = url["pk"]
+
+        content_len = int(self.headers.get("content-length", 0))
+        request_body = self.rfile.read(content_len)
+        request_body = json.loads(request_body)
+
+        if url["requested_resource"] == "categories":
+            if pk != 0:
+                successfully_updated = update_category(pk, request_body)
+                if successfully_updated:
+                    return self.response(
+                        "", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value
+                    )
+
+        return self.response(
+            "Requested resource not found",
+            status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value,
+        )
 
 
 def main():
